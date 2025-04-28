59a35a80f2300323015aa889a13b6d9dae541086
d13gu1n <d.acevedom1993@gmail.com>
2025-04-28 19:01:44 -0400

    Imported dotenv on quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -1,6 +1,7 @@
 // Configuration for your app
 // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file
 
+import "dotenv/config";
 import { defineConfig } from '#q-app/wrappers'
 
 export default defineConfig((ctx) => {
